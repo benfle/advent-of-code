23b81167817e3ef00a613f4288704486cd60bd32
Benoît Fleury <me@benfle.com>
2025-12-03 06:38:48 -0500

    day 3

---
+++ src/advent_of_code/2025/03.clj
@@ -0,0 +1,44 @@
+(ns advent-of-code.2025.03
+  "Day 3: Lobby
+
+  See https://adventofcode.com/2025/day/3"
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clojure.math :as math]
+            [advent-of-code.utils :refer [input]]))
+
+(def banks
+  (input 2025 3 str))
+
+(defn max-s
+  [s1 s2]
+  (if (or (nil? s1)
+          (< (int (first s1)) (int (first s2))))
+    s2
+    s1))
+
+(defn max-joltage
+  [bank n]
+  (loop [n n
+         digits (seq bank)
+         max-digits []]
+    (if (zero? n)
+      (parse-long (apply str max-digits))
+      (let [[next & xs] (->> (iterate rest digits)
+                             (take-while #(<= n (count %)))
+                             (reduce max-s nil))]
+        (recur (dec n)
+               xs
+               (conj max-digits next))))))
+
+(time
+ (assert 16887
+         (->> banks
+              (map #(max-joltage % 2))
+              (reduce + 0))))
+
+(time
+ (assert 167302518850275
+         (->> banks
+              (map #(max-joltage % 12))
+              (reduce + 0))))
